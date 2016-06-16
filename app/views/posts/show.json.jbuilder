20583e5c4aadbf1e1bbce0c6acc767b7009d2a81
wedsonlima <wedson.sousa.lima@gmail.com>
2016-06-16 15:47:32 -0300

    lesson1

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.extract! @post, :id, :title, :body, :created_at, :updated_at
+
+json.outra_coisa 'outra coisa aqui'
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'posts#index'
+
+  resources :posts do
+    get "admin/comments", to: 'posts#comments', from: 'admin'
+    get "user/comments", to: 'posts#comments', from: 'user'
+    get :comments, to: 'posts#comments', defaults: { format: :json }
+  end
+
+  namespace :admin do
+    resources :posts, only: :index, as: :rota_do_gilson
+  end
+end
